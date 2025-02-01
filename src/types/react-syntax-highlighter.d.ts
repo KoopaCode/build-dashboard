94dfbf254d2c3be99f3d19c3ad15c0ec532a4884
Koopa <115321970+KoopaCode@users.noreply.github.com>
2025-02-01 17:30:06 -0500

    Create react-syntax-highlighter.d.ts

---
+++ src/types/react-syntax-highlighter.d.ts
@@ -0,0 +1,38 @@
+declare module 'react-syntax-highlighter' {
+  export const PrismLight: any;
+  export const Prism: any;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/styles/prism' {
+  export const oneDark: any;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/languages/prism/yaml' {
+  const language: any;
+  export default language;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/languages/prism/typescript' {
+  const language: any;
+  export default language;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/languages/prism/javascript' {
+  const language: any;
+  export default language;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/languages/prism/json' {
+  const language: any;
+  export default language;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/languages/prism/markdown' {
+  const language: any;
+  export default language;
+}
+
+declare module 'react-syntax-highlighter/dist/cjs/languages/prism/java' {
+  const language: any;
+  export default language;
+} 
